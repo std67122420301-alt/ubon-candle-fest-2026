71c068e738d92417b54f6d17b9c529a17e16a79e
67122420301 <67122420301@ubru.ac.th>
2026-09-24 11:38:42 +0700

    Remove drop_all for production stability

--- server.py
+++ server.py
@@ -2,10 +2,9 @@ from ubcf import app, db
 from ubcf.models import Role
 
 with app.app_context():
-    db.drop_all()   # ลบตารางเก่า
-    db.create_all() # สร้างตารางใหม่
     
-    # เพิ่มข้อมูล Role เริ่มต้น หากในตารางยังไม่มีข้อมูล
+    #db.create_all()
+    
     if not Role.query.first():
         admin_role = Role(id=1, name='Admin')
         user_role = Role(id=2, name='User')
